146b87dcfbd3476ad1083e6cade55fad437c2f77
Andhar7 <70223638+Andhar7@users.noreply.github.com>
2020-12-15 20:28:19 +0100

    Initial Commit

---
+++ EncodeJson/EncodeJsonApp.swift
@@ -0,0 +1,17 @@
+//
+//  EncodeJsonApp.swift
+//  EncodeJson
+//
+//  Created by Andrej Kling on 15.12.20.
+//
+
+import SwiftUI
+
+@main
+struct EncodeJsonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
